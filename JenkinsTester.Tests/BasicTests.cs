b3283c827b1ee9b6ccaef37b86b6c171bde4d836
unknown <jsiegel@Persian14.fuzinteractive.com>
2015-07-31 11:37:51 -0400

    First commit of Jenkins Tester.

---
+++ JenkinsTester.Tests/BasicTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using JenkinsTester.Adders;
+
+namespace JenkinsTester.Tests
+{
+    [TestFixture]
+    public class BasicTests
+    {
+
+        public Adder adder;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            adder = new Adder();
+        }
+
+        [Test]
+        public void TestAdder()
+        {
+            int a = 1;
+            int b = 2;
+            int c = adder.AddNumbers(a,b);
+            Assert.AreEqual(3, c);
+        }
+
+        [Test]
+        public void TestBadAdder()
+        {
+            int a = 1;
+            int b = 2;
+            int c = adder.AddNumbersPoorly(a, b);
+            Assert.AreEqual(3, c);
+        }
+    }
+}
---
+++ JenkinsTester/Adder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTester.Adders
+{
+    public class Adder
+    {
+
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int AddNumbersPoorly(int a, int b)
+        {
+            return a + b + 1;
+        }
+
+
+    }
+}
